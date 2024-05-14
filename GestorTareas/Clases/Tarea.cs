feadd761692c25a61ec4bc37a1ea577ae45f9594
ByNoack <bynoackgames@gmail.com>
2024-05-13 19:48:34 -0500

    git ignore

---
+++ GestorTareas/Clases/Tarea.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorTareas.Clases
+{
+    internal class Tarea
+    {
+        private int tareaId;
+        private string titulo;
+        private string descripcion;
+        private DateTime fechaVencimiento;
+        private string prioridad;
+        private string estado;
+        private Usuario usuarioCreador; // Id del usuario que creó la tarea
+
+        public Tarea(string titulo, string descripcion, DateTime fechaVencimiento, string prioridad, string estado, Usuario usuarioCreador)
+        {
+            TareaId = GenerarIdAleatorio();
+            Titulo = titulo;
+            Descripcion = descripcion;
+            FechaVencimiento = fechaVencimiento;
+            Prioridad = prioridad;
+            Estado = estado;
+            UsuarioCreador = usuarioCreador;
+        }
+
+        public int TareaId { get => tareaId; private set => tareaId = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public DateTime FechaVencimiento { get => fechaVencimiento; set => fechaVencimiento = value; }
+
+        public string Prioridad
+        {
+            get => prioridad;
+            set
+            {
+                if (value == "Baja" || value == "Media" || value == "Alta")
+                    prioridad = value;
+                else
+                    throw new ArgumentException("El valor de prioridad no es válido.");
+            }
+        }
+
+        public string Estado
+        {
+            get => estado;
+            set
+            {
+                if (value == "Por Hacer" || value == "En Progreso" || value == "Terminado")
+                    estado = value;
+                else
+                    throw new ArgumentException("El valor de estado no es válido.");
+            }
+        }
+
+        public Usuario UsuarioCreador { get => usuarioCreador; set => usuarioCreador = value; }
+
+        private int GenerarIdAleatorio()
+        {
+            Random random = new Random();
+            return random.Next(1000, 10000); // Generar un ID aleatorio de 4 dígitos
+        }
+    }
+
+}
---
+++ GestorTareas/Clases/Usuario.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorTareas.Clases
+{
+    internal class Usuario
+    {
+        private int userId;
+        private string username;
+        private string email;
+        private string password;
+
+        public Usuario(string username, string email, string password)
+        {
+            UserId = GenerarIdAleatorio();
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+
+        public int UserId { get => userId; private set => userId = value; }
+
+        public string Username
+        {
+            get => username;
+            set
+            {
+                if (value.Length > 3)
+                    username = value;
+                else
+                    throw new ArgumentException("El nombre de usuario debe tener al menos 3 caracteres.");
+            }
+        }
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                if (value.Contains("@"))
+                    email = value;
+                else
+                    throw new ArgumentException("Esto no es un email válido.");
+            }
+        }
+
+        public string Password { get => password; set => password = value; }
+
+        private int GenerarIdAleatorio()
+        {
+            Random random = new Random();
+            return random.Next(1000, 10000); // Generar un ID aleatorio de 4 dígitos
+        }
+
+    }
+}
